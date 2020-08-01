b39cb2ca42e4d63bd740a55bf3e48b149ecf0613
Michal Zborowski <historykus@interia.pl>
2020-08-01 22:35:37 +0200

    changes

--- src/pages/Projekty.js
+++ src/pages/Projekty.js
@@ -3,8 +3,6 @@ import { Container, Col, Row } from 'react-bootstrap';
 import gthlogo from '../images/gth-logo.png';
 import splashimg from '../images/splash.png';
 
-
-
 const Projekty = () => {
     return (  
         
@@ -12,14 +10,13 @@ const Projekty = () => {
             <Row>
             <Col sm={6}>
             <div className="project1"><h3 className="text-in-shape-projects">Projekt #1</h3>< a href="https://zborowskidesign.github.io/Currency-Converter/#/" target="_blank"><img className="github-logo"src={gthlogo} alt="github-logo" data-value="github1"></img></a><img className="splashlogo" data-value="splash1" src={splashimg}></img></div>
-
             </Col>
 
             <Col sm={6}>
             <div className="project2"><h3 className="text-in-shape-projects">Projekt #2</h3>< a href="https://zborowskidesign.github.io/ProjectRockeAalfa.github.io/" target="_blank"><img className="github-logo"src={gthlogo} alt="github-logo" data-value="github2"></img></a><img className="splashlogo" data-value="splash2" src={splashimg}></img></div>
             </Col>
 
-            <Col sm={6} style={{zIndex:"-1"}}>
+            <Col sm={6}>
             <div className="project3"><h3 className="text-in-shape-projects">Projekt #3</h3>< a href ="https://zborowskidesign.github.io/speech-recognition/" target="_blank"><img className="github-logo"src={gthlogo} alt="github-logo" data-value="github3"></img></a><img className="splashlogo" data-value="splash3" src={splashimg}></img></div>
        
                  </Col>
@@ -29,7 +26,6 @@ const Projekty = () => {
       
                  </Col>
             </Row>
-  
         </Container>
         
     );
