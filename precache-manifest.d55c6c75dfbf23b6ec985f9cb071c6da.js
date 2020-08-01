93bd745951277feaafd4ea0515931526fc7f3af0
Michal Zborowski <historykus@interia.pl>
2020-08-01 19:16:48 +0200

    Updates

---
+++ precache-manifest.d55c6c75dfbf23b6ec985f9cb071c6da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8277d93c5717862a754f2a4f5546c431",
+    "url": "/Portfolio-ReactBootstrap/index.html"
+  },
+  {
+    "revision": "71755f5ff60e4cbbad20",
+    "url": "/Portfolio-ReactBootstrap/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8c50ce11da231287e729",
+    "url": "/Portfolio-ReactBootstrap/static/css/main.a341c7e1.chunk.css"
+  },
+  {
+    "revision": "71755f5ff60e4cbbad20",
+    "url": "/Portfolio-ReactBootstrap/static/js/2.c9a4235b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-ReactBootstrap/static/js/2.c9a4235b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c50ce11da231287e729",
+    "url": "/Portfolio-ReactBootstrap/static/js/main.40a8138d.chunk.js"
+  },
+  {
+    "revision": "368dd6caebdefcf2f5d1",
+    "url": "/Portfolio-ReactBootstrap/static/js/runtime-main.94cba378.js"
+  },
+  {
+    "revision": "e43c16d32d8b3501374646d8d43572eb",
+    "url": "/Portfolio-ReactBootstrap/static/media/corner-page.e43c16d3.png"
+  },
+  {
+    "revision": "b37fe2d6c5dcc51fcae95c83309c7577",
+    "url": "/Portfolio-ReactBootstrap/static/media/dotes.b37fe2d6.gif"
+  },
+  {
+    "revision": "313a51645017c14063a5cd8ac1dc039c",
+    "url": "/Portfolio-ReactBootstrap/static/media/me.313a5164.jpg"
+  },
+  {
+    "revision": "225603e1f56bca7366d112b090e01934",
+    "url": "/Portfolio-ReactBootstrap/static/media/splash.225603e1.png"
+  },
+  {
+    "revision": "9195fde3eec4d7f1ea6e5794af2a00cf",
+    "url": "/Portfolio-ReactBootstrap/static/media/waves.9195fde3.svg"
+  }
+]);
